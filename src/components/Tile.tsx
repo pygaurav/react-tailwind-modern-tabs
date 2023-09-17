3088b05c4def802fb08e0c797b6665090f8d3f41
Gaurav Bhardwaj <smartgaurav22@gmail.com>
2023-09-17 18:36:14 +0530

    Changed Tile classes

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -6,19 +6,23 @@ import { faStar } from "@fortawesome/free-solid-svg-icons";
 const Tile: FC<thing> = ({ imageUrl, title, ratings }) => {
   return (
     <div className="p-4 m-2 border border-gray-800 rounded-lg shadow-sm shadow-gray-800 hover:border-cyan-700 hover:shadow-md overflow-hidden transition-transform duration-700 delay-75 hover:scale-110 cursor-pointer">
-      <img className="w-40 h-40 rounded-lg mx-auto mb-2" src={imageUrl} alt={title} />
+      <img
+        className="w-40 h-40 2xl:w-40 2xl:h-40 xl:w-20 xl:h-20 lg:h-12 lg:w-12 md:h-8 md:w-8 sm:h-5 sm:w-5 rounded-lg mx-auto mb-2"
+        src={imageUrl}
+        alt={title}
+      />
       <div className="pl-4 m-2">
-      <div className="p-1 text-sm text-gray-200 truncate font-sans">
-        {title}
-      </div>
-      <div className="flex items-center text-sm">
-        <span className="text-gray-400 text-xs">{ratings}</span>{" "}
-        <FontAwesomeIcon
-          className="text-gray-400 mx-1"
-          fontSize={10}
-          icon={faStar}
-        />
-      </div>
+        <div className="p-1 text-sm text-gray-200 truncate font-sans">
+          {title}
+        </div>
+        <div className="flex items-center text-sm">
+          <span className="text-gray-400 text-xs">{ratings}</span>{" "}
+          <FontAwesomeIcon
+            className="text-gray-400 mx-1"
+            fontSize={10}
+            icon={faStar}
+          />
+        </div>
       </div>
     </div>
   );
